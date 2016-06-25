064396b3e475727041817e09b22a492c72ba62ce
shaunvxc <shaun@yipitdata.com>
2016-06-25 16:02:42 -0400

    add fn to toggle window split

--- .vig-config.el
+++ .vig-config.el
@@ -396,6 +396,34 @@
 
 (global-set-key (kbd "C-;")  'json-format)
 
+(defun toggle-window-split ()
+  ;; toggles a split from horizontal to vertical and vice-versa
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
 ;; bind 'dumb-jump-go to M-. (as it works a lot like 'find-tag
 ;; should really put inside a python hook as dumb jump does not work with c#
 (global-set-key (kbd "M-.")  'dumb-jump-go-autosave)
