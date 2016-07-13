ae096fc51220026c300b0966d2a5dcdb4d849145
shaunvxc <shaun@yipitdata.com>
2016-07-13 16:57:28 -0400

    remove unused tag fns

--- .vig-config.el
+++ .vig-config.el
@@ -249,34 +249,6 @@
 (setq org-log-done t)
 
 
-;; (defun create-tags (dir-name)
-;;      "Create tags file for DIR-NAME."
-;;      (interactive "DDirectory: ")
-;;      (eshell-command
-;;       (format "find %s -type f -name \"*.[ch]\" | etags -" dir-name)))
-
-
-;; (defadvice find-tag (around refresh-etags activate)
-;;   "Rerun etags and reload tags if tag not found and redo 'find-tag'.
-;; If buffer is modified, ask about save before running etags."
-;;   (let ((extension (file-name-extension (buffer-file-name))))
-;;     (condition-case err
-;; 	ad-do-it
-;;       (error (and (buffer-modified-p)
-;; 		  (not (ding))
-;; 		  (y-or-n-p "Buffer is modified, save it? ")
-;; 		  (save-buffer))
-;; 	     (er-refresh-etags extension)
-;; 	     ad-do-it))))
-
-
-;; (defun er-refresh-etags (&optional extension)
-;;   "Refresh etags for all files of EXTENSION.")
-;;   (shell-command (format "etags *.%s" (or extension "el")))
-;;   (let ((tags-revert-without-query t))  ; don't query, revert silently
-;;     (visit-tags-table default-directory nil))
-;; )
-
 (setq tramp-default-method "ssh")
 
 (global-set-key (kbd "C-x f") 'find-file-in-project)
